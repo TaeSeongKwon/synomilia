2b5871476b942d792f02af7727f3a1b26d3e9bbb
TaeSeongKwon <xotjd183@gmail.com>
2019-02-21 00:10:33 +0900

    initiate cli

---
+++ client/hdr/synomilia_cli.h
@@ -0,0 +1,5 @@
+#ifndef __SYNOMILIA_CLI_H__
+#define __SYNOMILIA_CLI_H__
+
+
+#endif
---
+++ client/src/synomilia_cli.cpp
@@ -0,0 +1,8 @@
+#include "synomilia_cli.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+    return 0;
+}
